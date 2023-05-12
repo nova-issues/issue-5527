cfbe846eeff7077105c71111253c4ed369867161
Mior Muhammad Zaki <crynobone@gmail.com>
2023-05-12 09:43:49 +0800

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(UserTableSeeder::class);
         $this->call(SnowflakeExampleSeeder::class);
     }
 }
